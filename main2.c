35f1c4596451a121e02d80dbf0b607feccb7e3b5
timikid <timikit@naver.com>
2020-10-08 17:04:25 +0900

    04

--- main2.c
+++ main2.c
@@ -2,18 +2,45 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int square (int a)
+int factorial(int n)
 {
-    return (a*a); // a = a*a Àû¿ëX 
+	int i;
+	int res=1;
+	
+	for(i=1;i<=n;i++)
+	{
+		res = res * i;
+	}
+   return res;
 }
 
+int combination(int n, int r)
+{
+	int n1, n2, n3;
+	n1 = factorial(n);
+	n2 = factorial(n-r);
+	n3 = factorial(r);
+	return(n1/n2/n3); 
+}
 
+int get_integer()
+{
+	int x;
+	printf("input an integer : ");
+	scanf("%i", &x);
+	return x;
+}
+
+ 
 int main(int argc, char *argv[]) {
 	
-	int a = 2;
-	a = square(a);
-	printf("a=%i\n", a);
-	 
+	int n,r,result;
+	n = get_integer();
+	r = get_integer();
+	
+	result = combination(n,r);
+	
+	printf("combination result is %i", result);
+	
 	return 0;
 }
